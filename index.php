ae1a9e69d9e551de9c2c2e82865b071c595cbafd
atulvarshney322-ship-it <atulvarshney322@gmail.com>
2025-10-16 11:55:06 +0530

    atul2

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+if (isset($_POST['send'])) {
+    // Collect form data
+    $name    = $_POST['name'];
+    $email   = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'atulvarshney5577@gmail.com';
+    $mail->Password = 'Atulvarshney@5577#';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+        
+    $mail->setFrom('atulvarshney5577@gmail.com', 'Mailer');
+    $mail->addAddress('atulvarshney5577@gmail.com');
+    $mail->Subject = 'Test Mail';
+    $mail->Body    = 'This is a test message.';
+        echo $name;
+    $mail->send();
+    echo 'Mail sent successfully';
+} catch (Exception $e) {
+    echo "Mail sending failed. Error: {$mail->ErrorInfo}";
+}
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,13 +81,13 @@
 </head>
 <body>
 
-  <form action="sendmail.php" method="POST">
+  <form  method="POST">
     <h2>Contact Us</h2>
     <input type="text" name="name" placeholder="Your Name" required />
     <input type="email" name="email" placeholder="Your Email" required />
     <input type="text" name="subject" placeholder="Subject" required />
     <textarea name="message" placeholder="Your Message" rows="5" required></textarea>
-    <button type="submit" name="send">Send Message</button>
+    <input type="submit" name="send" value="send">
   </form>
 
 </body>
