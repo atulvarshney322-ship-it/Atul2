53fbf678f00ed53d46cd4d49155a1fb4f1fa8deb
atulvarshney322-ship-it <atulvarshney322@gmail.com>
2025-10-16 12:10:03 +0530

    atul3

--- index.php
+++ index.php
@@ -14,26 +14,23 @@ if (isset($_POST['send'])) {
     $email   = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
-    
-    try {
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'atulvarshney5577@gmail.com';
-    $mail->Password = 'Atulvarshney@5577#';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-        
-    $mail->setFrom('atulvarshney5577@gmail.com', 'Mailer');
-    $mail->addAddress('atulvarshney5577@gmail.com');
-    $mail->Subject = 'Test Mail';
-    $mail->Body    = 'This is a test message.';
-        echo $name;
-    $mail->send();
-    echo 'Mail sent successfully';
-} catch (Exception $e) {
-    echo "Mail sending failed. Error: {$mail->ErrorInfo}";
-}
+
+    $to='atulvarshney5577@gmail.com';
+    $subject='Form Data';
+
+    $message1="Name:-".$name."\n"."Email:-".$email;
+
+
+    $header="From:-".$email;
+
+    if(mail($to,$subject,$message1,$header))
+    {
+        echo "mail Send";
+    }
+    else
+    {
+        echo "mail not send";
+    }
 }
 ?>
 
